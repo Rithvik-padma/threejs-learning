c212c727c81acc3bc1b0018d75b761a62770ab49
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-07-07 16:55:38 +0530

    Add empty controls and some small changes

---
+++ debugui/src/scripts.js
@@ -0,0 +1,49 @@
+import * as three from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import * as lil from 'lil-gui'
+import gsap from 'gsap'
+
+const scene = new three.Scene(window.innerWidth, window.innerHeight)
+const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight)
+const renderer = new three.WebGLRenderer({canvas: document.querySelector('canvas.webgl')})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+camera.position.z = 5
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.update()
+
+const gui = new lil.GUI()
+
+const box = new three.Mesh(new three.BoxGeometry(1, 1, 1), new three.MeshBasicMaterial({color: '#ff0000'}))
+scene.add(box)
+
+const animate = () => {
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(animate)
+}
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth/window.innerHeight
+    camera.updateProjectionMatrix()
+})
+
+let resizeButton = document.querySelector('.resize') 
+let resizeIcon = document.querySelector('.resize>i')
+
+resizeButton.addEventListener('click', ()=>{
+    if(!document.fullscreenElement){
+        document.body.requestFullscreen()
+        resizeIcon.classList.replace('fa-expand', 'fa-compress')
+    }
+    else{
+        document.exitFullscreen()
+        resizeIcon.classList.replace('fa-compress', 'fa-expand')
+    }
+})
+
+animate()
