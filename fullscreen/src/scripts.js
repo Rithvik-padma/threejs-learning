ad4bb28ab2118a852734d18f8e40b0329ac3c7e2
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-07-07 12:17:35 +0530

    resizing and pixel ratio

---
+++ fullscreen/src/scripts.js
@@ -0,0 +1,31 @@
+import * as three from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+const scene = new three.Scene(window.innerWidth, window.innerHeight)
+const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight)
+const renderer = new three.WebGLRenderer({canvas: document.querySelector('canvas.webgl')})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+camera.position.z = 5
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.update()
+
+const box = new three.Mesh(new three.BoxGeometry(1, 1, 1), new three.MeshBasicMaterial({color: '#ff0000'}))
+scene.add(box)
+
+const animate = () => {
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(animate)
+}
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth/window.innerHeight
+    camera.updateProjectionMatrix()
+})
+
+animate()
