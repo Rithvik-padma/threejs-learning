9930fe674dd5c7b59f7fb460d1825d01004a4c19
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-07-11 00:48:57 +0530

    Small changes

--- textures/src/scripts.js
+++ textures/src/scripts.js
@@ -1,17 +1,40 @@
 import * as three from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import brownDoor from '/textures/door/color.jpg'
+import minecraft from '/textures/minecraft.png'
+import chessboard from '/textures/checkerboard-8x8.png'
 
 const scene = new three.Scene(window.innerWidth, window.innerHeight)
 const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight)
 const renderer = new three.WebGLRenderer({canvas: document.querySelector('canvas.webgl')})
 
 renderer.setSize(window.innerWidth, window.innerHeight)
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 camera.position.z = 3
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 controls.update()
+
+const textureLoader = new three.TextureLoader()
+const doorImg = textureLoader.load(brownDoor)
+const minecraftImg = textureLoader.load(minecraft)
+const chessboardImg = textureLoader.load(chessboard)
+
+const box = new three.Mesh(new three.BoxGeometry(1, 1, 1, 100, 100), new three.MeshBasicMaterial({map : chessboardImg}))
+scene.add(box)
+
+// doorImg.repeat.x = 2
+// doorImg.repeat.y = 2
+// doorImg.wrapS = three.RepeatWrapping
+// doorImg.wrapT = three.RepeatWrapping
+chessboardImg.generateMipmaps = false
+chessboardImg.minFilter = three.NearestFilter
+chessboardImg.magFilter = three.NearestFilter
+// doorImg.rotation = Math.PI * 0.25
+// doorImg.center.x = 0.5
+// doorImg.center.y = 0.5
 
 const animate = () => {
     renderer.render(scene, camera)
