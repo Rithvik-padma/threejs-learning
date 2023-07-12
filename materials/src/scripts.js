88ada130bf96c87bb92f5807776a2646ff1d266f
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-07-12 11:35:11 +0530

    Adding files for the door material

--- materials/src/scripts.js
+++ materials/src/scripts.js
@@ -8,11 +8,51 @@ const renderer = new three.WebGLRenderer({canvas: document.querySelector('canvas
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-camera.position.z = 3
+camera.position.z = 2
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
 controls.update()
+
+// const ambientLight = new three.AmbientLight(0xffffff, 1.5)
+// scene.add(ambientLight)
+// ambientLight.position.z = 1
+
+const pointLight = new three.PointLight(0xffffff, 0.5)
+scene.add(pointLight)
+pointLight.position.set(0, 0, 5)
+
+const ambientLight = new three.AmbientLight(0xffffff, 0.6)
+scene.add(ambientLight)
+
+const tloader = new three.TextureLoader()
+const door = tloader.load('textures/door/color.jpg')
+const ambientOcclusion = tloader.load('textures/door/ambientOcclusion.jpg')
+const heightMapping = tloader.load('textures/door/height.jpg')
+const normal = tloader.load('textures/door/normal.jpg')
+const alpha = tloader.load('textures/door/alpha.jpg')
+const metalness = tloader.load('textures/door/metalness.jpg')
+const roughness = tloader.load('textures/door/roughness.jpg')
+
+three.ColorManagement.enabled = false
+renderer.outputColorSpace = three.LinearSRGBColorSpace
+door.colorSpace = three.SRGBColorSpace
+
+const material = new three.MeshStandardMaterial({side: three.DoubleSide, map: door})
+material.transparent = true
+material.aoMap = ambientOcclusion
+material.normalMap = normal
+material.alphaMap = alpha
+material.metalness = 0.45
+material.roughness = 0.65
+material.metalnessMap = metalness
+material.roughnessMap = roughness
+material.displacementMap = heightMapping
+material.displacementScale = 0.05
+material.aoMap = ambientOcclusion
+material.aoMapIntensity = 0.5
+
+const plane = new three.Mesh(new three.PlaneGeometry(1, 1, 100, 100), material)
+scene.add(plane)
 
 const animate = () => {
     renderer.render(scene, camera)
