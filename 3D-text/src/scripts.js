a4e19d984e19de8466e0a96762d67188593d54d2
Rithvik-padma <rithvik.padma@gmail.com>
2023-07-17 01:38:59 +0530

    Add the text

--- 3D-text/src/scripts.js
+++ 3D-text/src/scripts.js
@@ -1,5 +1,9 @@
 import * as three from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry.js'
+import {FontLoader} from 'three/examples/jsm/loaders/FontLoader.js'
+import matcap from '/matcap.png'
+
 
 const scene = new three.Scene(window.innerWidth, window.innerHeight)
 const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight)
@@ -8,12 +12,37 @@ const renderer = new three.WebGLRenderer({canvas: document.querySelector('canvas
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-camera.position.z = 5
+camera.position.z = 5.5
+
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 controls.update()
 
+const textureLoader = new three.TextureLoader()
+const texture = textureLoader.load(matcap)
+
+three.ColorManagement.enabled = false
+renderer.outputColorSpace = three.LinearSRGBColorSpace
+texture.colorSpace = three.SRGBColorSpace
+
+const fontLoader = new FontLoader()
+fontLoader.load('/Inconsolata.json', (font) => {
+    const textGeometry = new TextGeometry('Rithvik Padma', {
+        font,
+        size: 0.9,
+        height: 0.25,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelOffset: 0,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelSegments: 5,
+    }) 
+    const text = new three.Mesh(textGeometry, new three.MeshMatcapMaterial({matcap: texture}))
+    scene.add(text)
+    textGeometry.center()
+})
 
 
 window.addEventListener('resize', () => {
